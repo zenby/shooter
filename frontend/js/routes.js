7a6620134827bca73308eaaa22f2e676e22627fb
zenby <zenbysel@gmail.com>
2018-07-25 17:56:41 +0300

    change params at about part

--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -1,6 +1,7 @@
 import { initializeGame, startGame } from './main';
 import { getScoreFromDatabase } from './utils/requestUtils';
 import { game } from './main';
+import { BUFF_PARAMS, INTERVALS } from './constants';
 
 const spriteWrapper = document.querySelector('.sprite-wrapper');
 const gameContainer = document.querySelector('.game-container');
@@ -157,7 +158,6 @@ function generateGameContent() {
   <canvas></canvas>
   `
 }
-
 function generateRulesContent() {
   return `<div class="container margin-top">
             <h3>Rules:</h3>
@@ -172,14 +172,13 @@ function generateRulesContent() {
                 he changes it, current hero speed discard to minimum.
             </p>
             <h5>Enemies:</h5>
-            <p>There are several enemies: dummy enemies, who move only with start direction,
-                and smart enemy, who begin to pursue if see hero at some range. Smart
-                enemy has an ability to increase his size, speed and defense by eating
-                his tribesmen.
+            <p>There are several enemies: ice golems, who move only with start direction,
+                and mashrooms - smart enemy, who begin to pursue if see hero at some range. Mashrooms have
+                 an ability to increase his size, speed and defense by eating another mashroom.
             </p>
             <h5>Buffs:</h5>
-            <p>Average buff time is <code>20</code> seconds. Hero can activate buff if catches it. 
-                At the game every <code>8</code> seconds generates new buff and on the field can be only one buff
+            <p>Average buff time is <code>${BUFF_PARAMS.buffTime / 1000}</code> seconds. Hero can activate buff if catches it. 
+                At the game every <code>${INTERVALS.addBuffItem / 1000}</code> seconds generates new buff and on the field can be only one buff
                 item. Hero can stack buffs if catches more than one. There are several buffs at the game:</p>
             <p>
                 <img src="./img/sword.png"> - improve hero's gun size and damage. <span class="red">Red</span> line indicator 
@@ -194,7 +193,7 @@ function generateRulesContent() {
                 <span class="black">Black</span> line indicator appears when hero take this buff.
             </p>
             <h5>Lvl up and score:</h5>
-            <p>Every <code>10</code> seconds hero reach new level. With each new level at game field
+            <p>Every <code>${INTERVALS.lvlUp / 1000}</code> seconds hero reach new level. With each new level at game field
                 creates a stack of dummy enemies. Amount of creatures at the stack
                 is equalent to the new level. Also every new level the size of creatures
                 and speed of dummy enemies increases. Score is equalent to the time
